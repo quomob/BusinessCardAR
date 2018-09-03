7dccc4032111babffe72b8675716c46f0bcda6bd
systemMac <systemmac@systemmacnomacbook-puro.local>
2018-09-03 16:19:40 +0900

    Initial Commit

---
+++ BusinessCardARTests/BusinessCardARTests.swift
@@ -0,0 +1,34 @@
+//
+//  BusinessCardARTests.swift
+//  BusinessCardARTests
+//
+//  Created by systemMac on 2018/09/03.
+//  Copyright © 2018年 systemMac. All rights reserved.
+//
+
+import XCTest
+@testable import BusinessCardAR
+
+class BusinessCardARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
